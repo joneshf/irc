89f8e8be8de3edf14bf6be46b85017b9b62c2a3a
joneshf <joneshf@imail.losrios.edu>
2012-08-15 19:00:15 -0700

    TinySong.
    Implemented tinysong command.
    Does not handle all the cases though.
    Error commands just come straight through.

--- src/bot.erl
+++ src/bot.erl
@@ -3,11 +3,12 @@
 
 -export([connect/0, loop/1]).
 
--define(COMMANDS, ["help"]).
--define(CHANNELS, ["#confab"]).
+-define(COMMANDS, ["help", "ts"]).
+-define(CHANNELS, ["#confab", "#python-forum"]).
 -define(HOST, "chat.freenode.net").
--define(NICK, "confabbot").
+-define(NICK, "confabot").
 -define(PORT, 6667).
+-define(TS_API, "api key").
 
 close(Socket) ->
     gen_tcp:close(Socket).
@@ -16,6 +17,8 @@ connect() ->
     {ok, Socket} = gen_tcp:connect(?HOST, ?PORT, [{active, false}]),
     nick(Socket, ?NICK),
     user(Socket, "bot bot bot :bot"),
+    io:format("Starting inets~n"),
+    inets:start(),
     io:format("Starting loop~n"),
     loop(Socket).
 
@@ -49,10 +52,39 @@ parse(Socket, [["PING", Reply]|Tail]) ->
 parse(Socket, [[_User, "PRIVMSG", Channel|Message]|Tail]) ->
     io:format("Recieved: PRIVMSG ~p~n", [Message]),
     case Message of
-	[">help"|_Tail] ->
-	    say(Socket, Channel,
-		"Here's what I can do: " ++ string:join(?COMMANDS, ", ")),
-	    parse(Socket, Tail);
+	[">help"|Rest] ->
+	    case Rest of
+		["ts"|_] ->
+		    say(Socket, Channel, "Search for music by album, artist," ++
+			    " or song");
+		_ ->
+		    say(Socket, Channel, "Prefix commands with '>'"),
+		    say(Socket, Channel, "Here's what I can do: " ++
+			    string:join(?COMMANDS, ", ")),
+		    parse(Socket, Tail)
+	    end;
+	[">ts"|Query] ->
+	    QuotedQuery = string:join(Query, "+"),
+	    Url = ("http://tinysong.com/a/" ++ QuotedQuery ++ "?format=json" ++
+		   "&key=" ++ ?TS_API),
+	    {ok, RequestId} = httpc:request(get, {Url, []}, [],
+					    [{sync, false}]),
+	    receive
+		{http, {RequestId, {_Resp, _Head, TsUrl}}} ->
+		    case TsUrl of
+			<<"[]">> ->
+			    say(Socket, Channel, "No matches found");
+			_ ->
+			    Escaped = binary:replace(TsUrl,
+						     [<<"\"">>, <<"\\">>],
+						     <<"">>, [global]),
+			    say(Socket, Channel, binary_to_list(Escaped))
+                    end;
+		{error, Error} ->
+		    io:format("Error: ~p~n", [Error])
+	    after 5000 ->
+		timeout
+	    end;
 	_ ->
 	    parse(Socket, Tail)
     end;
