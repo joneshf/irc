28e81532819c5b8d18a52e794f2df26af3a7f9ab
joneshf <joneshf@imail.losrios.edu>
2012-08-16 16:39:49 -0700

    Help and tiny_song.
    Implemented these two commands.

--- src/otp_bot.erl
+++ src/otp_bot.erl
@@ -5,19 +5,16 @@
 
 -export([code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1,
          terminate/2]).
--export([start_link/0, connect/0]).
+-export([start_link/0, connect/0, loop/1]).
 
--define(HOST, "chat.freenode.net").
--define(PORT, 6667).
--define(CHANNELS, ["#confab"]).
--define(NICK, "confabbot").
--define(USER, "bot bot bot :bot").
+-include("../include/otp_bot.hrl").
 
 %%% bot api
 start_link() ->
     gen_server:start_link(?MODULE, connect, []).
 
 connect() ->
+    inets:start(),
     case gen_tcp:connect(?HOST, ?PORT, [{active, false}]) of
         {ok, Socket} ->
             % Set up the nick and user.
@@ -28,6 +25,11 @@ connect() ->
         {error, Error} ->
             io:format("Error: ~p~n", [Error])
     end.
+
+help(["ts"|_Rest]) ->
+    "Search for music by album, artist, or song";
+help(_) ->
+    "Here's what I can do: " ++ string:join(?COMMANDS, ", ").
 
 %%% private
 join(Socket, ChannelList) ->
@@ -59,6 +61,22 @@ parse(Socket, [["PING", Reply]|Tail]) ->
 parse(Socket, [[Hostmask, "PRIVMSG", Channel|Message]|Tail]) ->
     [$:|Nick] = string:sub_word(Hostmask, 1, $!),
     io:format("~s - ~s: ~s~n", [Channel, Nick, string:join(Message, " ")]),
+    case Message of
+        [":>help"|Rest] ->
+            HelpMessage = help(Rest),
+            say(Socket, Channel, HelpMessage);
+        [":>ts"|Query] ->
+            TsMessage = tiny_song(Query),
+            case TsMessage of
+                {list, Songs} ->
+                    lists:foreach(fun(S) -> say(Socket, Channel, S) end,
+                                  lists:sublist(Songs, 3));
+                _ ->
+                    say(Socket, Channel, TsMessage)
+            end;
+        _ ->
+            parse(Socket, Tail)
+    end,
     parse(Socket, Tail);
 parse(Socket, [[_Server, "376"|_Message]|Tail]) ->
     io:format("Received end of MOTD, joining channel list~n"),
@@ -75,8 +93,41 @@ parse(Socket, [Line|Tail]) ->
 pong(Socket, Message) ->
     send(Socket, "PONG " ++ Message).
 
+say(Socket, Channel, Message) ->
+    io:format("Sending: PRIVMSG ~p :~p~n", [Channel, Message]),
+    send(Socket, "PRIVMSG " ++ Channel ++ " :" ++ Message).
+
 send(Socket, Message) ->
     gen_tcp:send(Socket, Message ++ "\r\n").
+
+tiny_song(Query) ->
+    QuotedQuery = mochiweb_util:quote_plus(string:join(Query, " ")),
+    Url = string:join(["http://tinysong.com/s/", QuotedQuery,
+                       "?format=json&key=", ?TS_API_KEY], ""),
+    io:format("Query: ~p~n", [Url]),
+    io:format("Better? ~p~n", [mochijson:encode(QuotedQuery)]),
+    {ok, RequestId} = httpc:request(get, {Url, []}, [], [{sync, false}]),
+    receive
+        {http, {RequestId, {_Resp, _Header, TsJson}}} ->
+            case mochijson:decode(TsJson) of
+                {array, []} ->
+                    "No matches found";
+                {array, Songs} ->
+                    {list, [Song ++ " by " ++ Artist ++ " - " ++ Link ||
+                               {struct, [{_L, Link}, _Sid, {_S, Song}, _Artid,
+                                         {_A, Artist}, _Albid, _Alb]}
+                                   <- Songs]};
+                {struct, [{"error", Error}]} ->
+                    io:format("Error: ~p~n", [Error]),
+                    "Wtf?";
+                Url ->
+                    Url
+            end;
+        {error, Error} ->
+            io:format("Error: ~p~n", [Error])
+    after 5000 ->
+        timeout
+    end.
 
 user(Socket, User) ->
     send(Socket, "USER " ++ User).
